2a94e5592c433f8129793f3404f6467a0c765ba6
andreneery <neryandre8@gmail.com>
2023-05-15 23:24:28 -0300

    adicionando lógica no vendasController

--- backend/trabalho-veiculos/app/Http/Controllers/VendasController.php
+++ backend/trabalho-veiculos/app/Http/Controllers/VendasController.php
@@ -7,43 +7,70 @@ use Illuminate\Http\Request;
 
 class VendasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $vendas = VendaModel::all();
+        return response()->json($vendas);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function create()
     {
-        //
+        $venda = new VendaModel();
+        $venda->veiculo_id = $request->input('veiculo_id');
+        $venda->cliente_id = $request->input('cliente_id');
+        $venda->valor = $request->input('valor');
+        $venda->data_venda = today();
+
+        $venda->save();
+        return response()->json($venda);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(VendaModel $vendaModel)
     {
-        //
+        //show a venda by id in json format
+        $findVenda = VendaModel::find($vendaModel);
+        if(!$findVenda){
+            return response()->json([
+                'message'   => 'Venda não encontrada',
+            ], 404);
+        }
+        return response()->json($findVenda);
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, VendaModel $vendaModel)
     {
-        //
+        //update a venda by id
+        $findVenda = VendaModel::find($vendaModel);
+        if(!$findVenda){
+            return response()->json([
+                'message'   => 'Venda não encontrada',
+            ], 404);
+        } else {
+            $findVenda->veiculo_id = $request->input('veiculo_id');
+            $findVenda->cliente_id = $request->input('cliente_id');
+            $findVenda->valor = $request->input('valor');
+            $findVenda->data_venda = today();
+
+            $findVenda->save();
+
+            return response()->json($findVenda);
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(VendaModel $vendaModel)
     {
-        //
+        //delete a venda by id
+        $findVenda = VendaModel::find($vendaModel);
+        if(!$findVenda){
+            return response()->json([
+                'message'   => 'Venda não encontrada',
+            ], 404);
+        } else {
+            $findVenda->delete();
+            return response()->json([
+                'message'   => 'Venda deletada com sucesso',
+            ], 200);
+        }
     }
 }
